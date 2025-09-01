0900c63299c9fb20605218ac72cf94e138475f47
dev <almas1610@gmail.com>
2025-09-02 04:53:04 +0500

    feature(API): create http instance with axios and configure proxy server

---
+++ app/lib/api.ts
@@ -0,0 +1,40 @@
+import axios, {
+  AxiosError,
+  AxiosInstance,
+  AxiosResponse,
+  CreateAxiosDefaults,
+} from "axios";
+
+const defaultOptions: CreateAxiosDefaults = {
+  baseURL: process.env.API_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+const http: AxiosInstance = axios.create(defaultOptions);
+
+http.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    console.error("API Error: ", error.response?.data || error.message);
+    return Promise.reject(error);
+  }
+);
+
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+const request = {
+  get: <T>(url: string) => http.get<T>(url).then(responseBody),
+  post: <T>(url: string, body: {}) =>
+    http.post<T>(url, body).then(responseBody),
+};
+
+const article = {
+  list: async () => {},
+  byId: async (id: number) => {},
+  listByCategory: async (categoryId: number) => {},
+};
+
+export { article };
--- vite.config.ts
+++ vite.config.ts
@@ -21,4 +21,13 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+    },
+  },
 });
