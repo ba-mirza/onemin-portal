2b75823b2ce70b56a522f12ed67b4ee6bd0fb68c
dev <almas1610@gmail.com>
2025-09-02 03:49:56 +0500

    init(project): Initialize omk-portal with Remix

---
+++ app/routes/_index.tsx
@@ -0,0 +1,16 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div>
+      <h1 className="text-center mt-6 text-3xl">Index page</h1>
+    </div>
+  );
+}
