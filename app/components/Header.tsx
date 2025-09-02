f953c0eca4eed0dd9270faac84a5dbeaeb0f7d3a
dev <almas1610@gmail.com>
2025-09-02 06:26:34 +0500

    feature(lib): add shadcn lib for UI and updated Header button

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,13 @@
+import { Button } from "./ui/button";
+
 export default function Header() {
     return (
         <div className="m-auto md:w-[1300px] mt-8">
             <div className="flex items-center md:justify-between">
                 <h1 className="uppercase text-2xl">onemin.kz</h1>
-                <button>GET NOTIFY</button>
+                <Button variant="destructive" className="uppercase">Получать рассылку</Button>
             </div>
-            <div className="md:w-full h-[74px] bg-[#202020] md:rounded-lg">
+            <div className="mt-2 md:w-full h-[74px] bg-[#202020] md:rounded-lg">
             </div>
         </div>
     )
